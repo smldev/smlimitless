227735dfbafd1c9fb20bc7052c626def50316fd3
Celarix <celarix@stardustfields.net>
2013-01-05 11:33:48 -0500

    Added GameServices type and Graphics namespace.

---
+++ SMLimitless/SMLimitless/SMLimitless/GameServices.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SMLimitless
+{
+    public static class GameServices
+    {
+        private static GameServiceContainer container;
+        public static GameServiceContainer Container
+        {
+            get
+            {
+                if (container == null)
+                {
+                    container = new GameServiceContainer();
+                }
+                return container;
+            }
+        }
+
+        public static void InitializeServices(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            AddService<GraphicsDevice>(graphicsDevice);
+            AddService<SpriteBatch>(spriteBatch);
+            AddService<GameTime>(gameTime);
+        }
+
+        public static GraphicsDevice Graphics
+        {
+            get { return GetService<GraphicsDevice>(); }
+        }
+
+        public static SpriteBatch SpriteBatch
+        {
+            get { return GetService<SpriteBatch>(); }
+        }
+
+        public static GameTime GameTime
+        {
+            get { return GetService<GameTime>(); }
+        }
+
+        public static T GetService<T>()
+        {
+            return (T)Container.GetService(typeof(T));
+        }
+
+        public static void AddService<T>(T service)
+        {
+            Container.AddService(typeof(T), service);
+        }
+
+        public static void RemoveService<T>()
+        {
+            Container.RemoveService(typeof(T));
+        }
+    }
+}
--- SMLimitless/SMLimitless/SMLimitless/SmlProgram.cs
+++ SMLimitless/SMLimitless/SMLimitless/SmlProgram.cs
@@ -1,6 +1,8 @@
+#region Using Statements
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -8,9 +10,13 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+#endregion
 
 namespace SMLimitless
 {
+    /// <summary>
+    /// The main type for the game.
+    /// </summary>
     public sealed class SmlProgram : Game
     {
         GraphicsDeviceManager graphics;
